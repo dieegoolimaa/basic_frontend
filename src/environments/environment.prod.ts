0537d2e758538e21188a8621820c9724a3c795cb
Diego Fernandes <dieegoolimaa@gmail.com>
2025-12-12 15:46:24 +0000

    update url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
     production: true,
     // TODO: Update this URL with your Render backend URL
     // Example: https://basic-backend.onrender.com/api
-    apiUrl: 'https://basic-backend.onrender.com/api',
+    apiUrl: 'https://basic-backend-0ims.onrender.com/api',
 };
